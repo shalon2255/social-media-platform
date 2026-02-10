cb47f9e0321bce293c784f1c8cc89fdc4a7d1ef3
sharonempire <sharon@empireoe.com>
2026-02-10 23:40:22 +0530

    fixed import case sensitivity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Register from './pages/Register'
 import Feed from './pages/feed'
 import CreatePost from './pages/CreatePost'
