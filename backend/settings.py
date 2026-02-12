4dfe8fcf398f0442a78172e31c8fde4d5730d08c
sharonempire <sharon@empireoe.com>
2026-02-12 22:26:31 +0530

    committed

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,7 @@ SECRET_KEY = 'django-insecure--m0(yq)pb(y!9&9(79w9z$479vp2s)f+^a(b_p(3x*a(93s6g&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["https://lunex-backendd.onrender.com",
-                  "localhost",
-    "127.0.0.1",]
+ALLOWED_HOSTS = ["*"] 
 CSRF_TRUSTED_ORIGINS = [
     "https://lunex-backendd.onrender.com",
     "http://localhost:5173",
